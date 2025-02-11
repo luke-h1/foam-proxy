35036ac8200760dcfdcf7ec40a12f1b0e262a4e4
luke-h1 <luke.howsam@yahoo.com>
2025-02-11 17:12:44 +0000

    chore(lambda): fix build

--- src/handlers/health/index.ts
+++ src/handlers/health/index.ts
@@ -1,11 +1,10 @@
 const healthHandler = () => {
-    return JSON.stringify(
-      {
-        status: 'OK',
-      },
-      null,
-      2,
-    );
-  };
-  export default healthHandler;
-  
+  return JSON.stringify(
+    {
+      status: 'OK',
+    },
+    null,
+    2,
+  );
+};
+export default healthHandler;
