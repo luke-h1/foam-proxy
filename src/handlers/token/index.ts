7767a77558b7e85fe6091c9b0d48fe20aa10e098
luke-h1 <luke.howsam@yahoo.com>
2025-02-18 21:07:51 +0000

    chore(lambda): debug lambda

--- src/handlers/token/index.ts
+++ src/handlers/token/index.ts
@@ -3,18 +3,9 @@ import axios from 'axios';
 export default async function tokenHandler() {
   try {
     const { data } = await axios.post(
-      'https://id.twitch.tv/oauth2/token',
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`,
       null,
-      {
-        params: {
-          client_id: process.env.TWITCH_CLIENT_ID,
-          client_secret: process.env.TWITCH_CLIENT_SECRET,
-          grant_type: 'client_credentials',
-        },
-        headers: {
-          'Content-Type': 'x-www-form-urlencoded',
-        },
-      },
+      {},
     );
     return JSON.stringify({
       data,
