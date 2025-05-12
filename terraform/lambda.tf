72a3f19e37bf915a2b401bc1ada5e5ff838219a6
luke-h1 <luke.howsam@yahoo.com>
2025-05-12 10:48:50 +0100

    feat(lambda): add authorizer

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,6 +1,6 @@
 data "archive_file" "lambda_archive" {
   type        = "zip"
-  source_dir  = "${path.module}/../dist"
+  source_dir  = "${path.module}/../apps/foam-authorizer/dist"
   output_path = "${path.module}/../lambda.zip"
 }
 
