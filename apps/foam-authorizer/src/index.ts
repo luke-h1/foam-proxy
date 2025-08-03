96cdde2e9bc03a421980d4ba2f5d871c5f85ab57
luke-h1 <luke.howsam@yahoo.com>
2025-08-03 17:55:21 +0100

    fix(authorizer): protect with api key

--- apps/foam-authorizer/src/index.ts
+++ apps/foam-authorizer/src/index.ts
@@ -19,10 +19,7 @@ export const handler = (
       throw new Error('Blank API key');
     }
 
-    if (apiKey.trim() !== process.env.API_KEY?.trim()) {
-      console.error(
-        `Unauthorized attempt to access foam proxy with key: ${apiKey}`,
-      );
+    if (apiKey !== process.env.API_KEY) {
       return generatePolicy('user', 'Deny', event.methodArn);
     }
 
