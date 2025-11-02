6736465019cc1795dc6e58ae7a743590fbab998c
luke-h1 <luke.howsam54@gmail.com>
2025-11-02 13:37:29 +0000

    fix(lambda): sentry

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -3,6 +3,7 @@ resource "aws_dynamodb_table" "dynamodb_terraform_lock" {
   hash_key       = "LockID"
   read_capacity  = 20
   write_capacity = 20
+  billing_mode = "PAY_PER_REQUEST"
   attribute {
     name = "LockID"
     type = "S"
