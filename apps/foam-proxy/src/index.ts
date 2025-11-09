d322912f7aafc01722530fc2249d52b5b154cac5
luke-h1 <luke.howsam54@gmail.com>
2025-11-09 20:48:12 +0000

    feat(lambda): sentry monitoring

--- apps/foam-proxy/src/index.ts
+++ apps/foam-proxy/src/index.ts
@@ -1,39 +1,47 @@
 /* eslint-disable no-console */
 import { APIGatewayProxyEvent, Context, Handler } from 'aws-lambda';
+import * as Sentry from '@sentry/serverless';
 import routes from './routes';
 import lambdaTimeout from './util/lambdaTimeout';
 
-export const handler: Handler = async (
-  event: APIGatewayProxyEvent,
-  context: Context,
-) => {
-  const { path } = event;
+Sentry.AWSLambda.init({
+  dsn: process.env.SENTRY_PROXY_DSN,
+  environment: process.env.SENTRY_ENVIRONMENT,
+  release: process.env.SENTRY_RELEASE,
+  tracesSampleRate: 0.5,
+});
 
-  const { queryStringParameters } = event;
+export const handler: Handler = Sentry.AWSLambda.wrapHandler(
+  async (event: APIGatewayProxyEvent, context: Context) => {
+    const { path } = event;
 
-  const queryString = new URLSearchParams(
-    queryStringParameters as unknown as string,
-  ).toString();
+    const { queryStringParameters } = event;
 
-  const url = `https://${event.headers.Host}${event.path}?${queryString}`;
+    const queryString = new URLSearchParams(
+      queryStringParameters as unknown as string,
+    ).toString();
 
-  console.info('origin url ->', url);
+    const url = `https://${event.headers.Host}${event.path}?${queryString}`;
 
-  try {
-    return await Promise.race([
-      routes(path, queryStringParameters, url),
-      lambdaTimeout(context),
-    ]).then(value => value);
-  } catch (e) {
-    return {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: {
-        error: e,
-      },
-    };
-  }
-};
+    console.info('origin url ->', url);
+
+    try {
+      return await Promise.race([
+        routes(path, queryStringParameters, url),
+        lambdaTimeout(context),
+      ]).then(value => value);
+    } catch (e) {
+      Sentry.captureException(e);
+      return {
+        statusCode: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: {
+          error: e,
+        },
+      };
+    }
+  },
+);
